6c0884c364ac692457bc4feb82847bc164331441
kingsman20 <kingslifeudo@gmail.com>
2020-07-30 00:35:22 +0100

    Added user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [CarsModule, DatabaseModule],
+  imports: [
+    CarsModule,
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
